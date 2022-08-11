00e8bc7738bf2f8fa900538f219ca2e31bbcd076
DaniK178 <daniellekyere@gmail.com>
2022-08-11 20:37:43 +0100

    mvp completed

--- src/main/java/SecretNumber.java
+++ src/main/java/SecretNumber.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class SecretNumber {
 
     public static void main(String[] args) {
-//        put code here
+
+        System.out.println("Welcome to guess number! The secret number lies between 0 and 50.");
+        System.out.println("Type your guess below:");
+
+//        collect user input
+        Scanner scan = new Scanner(System.in);
+        Integer num = scan.nextInt();
+
+//        if number is 26, print 'correct!'
+//        if number is less the 26 print 'Too low!'
+//        if number is more than 26 print 'Too high!"
+
+        if (num == 26) {
+            System.out.println("correct!");
+        } else if (num < 26) {
+            System.out.println("Too low! Try a higher number.");
+        } else {
+            System.out.println("Too high! Try a lower number.");
+        }
+
     }
 }
--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class WhatToWear {
 
     public static void main(String[] args) {
 //         put code here
+//        ask what is the weather is like. and put if to their answer
+        System.out.println("What is the weather like outside?");
+
+        Scanner reader = new Scanner(System.in); // creates a Scanner object
+        String weather = reader.nextLine();
+
+        if (favouriteChocolate.equals("Bounty")){
+            System.out.println("gross");
+        } else if (favouriteChocolate.equals("Crunchie")) {
+            System.out.println("The best!");
+        } else if (){
+
+        } else {
+            System.out.println("yum");
+        }
+
+        boolean 8 = true;
+        boolean 14 = false;
+        boolean 28= false;
     }
 }
